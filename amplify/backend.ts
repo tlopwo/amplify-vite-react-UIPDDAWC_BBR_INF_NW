e3e5e62dfb1ca6393680e3b820268597560615ce
Mehirion <Mehirion1@gmail.com>
2024-11-26 19:42:07 +0100

    poprawka kodu

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,23 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { downloadData } from 'aws-amplify/storage';
 
 defineBackend({
   auth,
   data,
   storage
 });
+
+
+try {
+  const result = downloadData({
+    path: "album/2024/1.jpg",
+    options: {
+      // Specify a target bucket using name assigned in Amplify Backend
+      bucket: "secondBucket"
+    }
+  }).result;
+} catch (error) {
+  console.log(`Error: ${error}`)
+}
