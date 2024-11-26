4d80e8edd7661a8d610c9618c1433a72e78037aa
Mehirion <Mehirion1@gmail.com>
2024-11-26 19:24:29 +0100

    Dodanie storage

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,5 +1,9 @@
 import { defineAuth } from '@aws-amplify/backend';
+import { defineStorage } from '@aws-amplify/backend';
 
+export const storage = defineStorage({
+  name: 'amplifyTeamDrive'
+});
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
